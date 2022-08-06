7f88cf81b396924a86f4cc0443e39460b0a00489
stephen <stephn2027@gmail.com>
2022-08-06 16:31:35 +0900

    added mutation to add a product

--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c21f84d95c58655efa08";
+/******/ 	var hotCurrentHash = "bcdfc387a4e2ae17dcb7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
