92b50e392ba4a7accb7ce9f4d74a97c3348bf813
stephen <stephn2027@gmail.com>
2022-08-26 13:42:24 +0900

    added delete function and query. Used apollo cache evict to refresh cache for deleted items

--- frontend/.next/server/pages/sell.js
+++ frontend/.next/server/pages/sell.js
@@ -162,7 +162,7 @@ module.exports = require("styled-components");;
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-var __webpack_exports__ = __webpack_require__.X(0, ["vendors-node_modules_next_dist_client_request-idle-callback_js-node_modules_next_node_modules-847d74","vendors-node_modules_next_dist_client_link_js","components_ErrorMessage_js","components_Products_js"], function() { return __webpack_exec__("./pages/sell.js"); });
+var __webpack_exports__ = __webpack_require__.X(0, ["vendors-node_modules_next_dist_client_request-idle-callback_js-node_modules_next_node_modules-847d74","vendors-node_modules_next_dist_client_link_js","components_Products_js"], function() { return __webpack_exec__("./pages/sell.js"); });
 module.exports = __webpack_exports__;
 
 })();
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9828a6e92666f63c46d5"; }
+/******/ 		__webpack_require__.h = function() { return "41e5163bd05aee68fa4a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
